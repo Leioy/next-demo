c35383ea49bd8e01eec12056debb501830168262
Leioy <13902303081@139.com>
2020-06-25 01:33:24 +0800

    使用ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,29 @@
-import Head from 'next/head'
 import React from 'react'
-import Link from 'next/link'
+import { GetServerSideProps, NextPage } from 'next'
+import { UAParser } from 'ua-parser-js'
 
-import png from 'assets/images/1.png'
-
-
-export default function Home () {
+type Props = {
+	browser: {
+		name: string
+		version: string
+		major: string
+	}
+}
+const Index: NextPage<Props> = (props) => {
+	const { browser } = props
 	return (
 		<div className="container">
-			<h1>标题1</h1>
-			<p>段落</p>
-			<img src={png} alt=""/>
-			
-			<style jsx>{`
-				h1 {
-				color: blue;
-				}
-			`}</style>
+			<h1>你的浏览器是{browser.name}</h1>
 		</div>
 	)
+}
+export default Index
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const ua = context.req.headers['user-agent']
+	const result = new UAParser(ua).getResult()
+	return {
+		props: {
+			browser: result.browser,
+		},
+	}
 }
